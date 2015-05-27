5b70df1fe983953b1c60bf441eaf7879b02b24a7
Ryan Docherty <ryandocherty95@gmail.com>
2015-05-27 12:46:31 +0100

    Fake Info Arrays
    Simple arrays of fake info

---
+++ FacialRecognition/FakeInfoGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiFaceRec
+{
+    class FakeInfoGenerator
+    {
+        public string[] fakeName = new string[5] { "Natasha", "Joseph", "Max", "Joshua", "Caitlin" };
+        public int[] fakeAge = new int[5] { 18, 23, 25, 30, 40 };
+        public string[] fakeAddress = new string[5] { "60 Academy Street, BENTLEY, HU17 0QQ", "89 Sandyhill Rd, FRITTENDEN, TN17 2TF", "4 Albert Embankment, Lambeth, London SE1 7SR", "5 Mandela Way, London SE1 5GG, UK", "56 Empire Square East, London SE1 4NB, UK" };
+        public string[] fakeNumber = new string[5] { "079 8749 0219", "078 0115 4223", "078 0932 6276", "077 5936 3722", "077 5936 3122" };
+        public string[] fakeEmail = new string[5] { "Howething@hotmail.com", "flippityflop@gmail.com", "zachiscool95@dayrep.com", "colabthings@hotmail.co.uk", "natasha891@dgmail.com" };
+        public string[] fakeHeight = new string[5] { "168cm", "153cm", "173cm", "180cm", "160cm" };
+        
+        
+
+       
+    }
+}
